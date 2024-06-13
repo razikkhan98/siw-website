47060cd2620e34dda1320c63a14a2910e3f7f5ce
razikkhan98 <razik.khan@skillintelligence.tech>
2024-06-13 15:11:11 +0530

    working Service pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import "./App.css";
-import Home from "./Component/Pages/Home/index";
-import Footer from "./Component/Pages/footer";
-import Team from "./Component/Pages/team";
-import Service from "./Component/Pages/service";
-import FAQ from "./Component/Pages/faq";
+// import Home from "./Component/Pages/Home/index";
+// import Footer from "./Component/Pages/footer";
+// import Team from "./Component/Pages/team";
+import Service from "./Component/Pages/Service/index";
+// import FAQ from "./Component/Pages/faq";
 import About from "./Component/Pages/About";
 const App = () => {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
       <About/>
-      <Team />
+      {/* <Team /> */}
       <Service />
-      <FAQ />
-      <Footer />
+      {/* <FAQ /> */}
+      {/* <Footer /> */}
     </>
   );
 };
--- src/Component/Pages/About/index.jsx
+++ src/Component/Pages/About/index.jsx
@@ -1,145 +1,149 @@
 import React from "react";
 
 // Images
-import Abt1 from "../../assets/img/abt-1.jpg";
-import Abt2 from "../../assets/img/abt-2.jpg";
-import Abt3 from "../../assets/img/abt-3.jpg";
+import Abt1 from "../../assets/img/About/abt-1.jpg";
+import Abt2 from "../../assets/img/About/abt-2.jpg";
+import Abt3 from "../../assets/img/About/abt-3.jpg";
 
 // Icons
-import { FaPlay, FaFileDownload, FaUser, FaImage, FaUsersCog, FaThumbsUp, FaUsers, FaTrophy} from "react-icons/fa";
+import {
+  FaPlay,
+  FaFileDownload,
+  FaUser,
+  FaImage,
+} from "react-icons/fa";
 
 const About = () => {
   const AboutCard = [
     {
       icon: <FaPlay />,
-      title:'Responsive'
+      title: "Responsive",
     },
     {
       icon: <FaFileDownload />,
-      title:'Free Download'
+      title: "Free Download",
     },
     {
       icon: <FaUser />,
-      title:'Support'
+      title: "Support",
     },
     {
       icon: <FaImage />,
-      title:'Development'
+      title: "Development",
     },
-  ]
+  ];
   return (
     <>
       <div className="background-color-sky-blue">
-        <div class="container mx-auto">
-          <div class="grid lg:grid-cols-2 gap-2 py-48">
-            <div class="px-6">
-              <div class="flex flex-wrap items-center">
-                <div class="w-1/2">
-                  <div class="flex flex-col">
-                    <div class="w-full mt-4 pt-2">
-                      <div class="shadow-img bg-white rounded shadow-xl overflow-hidden mr-5">
-                        <img src={Abt1} class="w-full" alt="Loading" />
-                        <div class="bg-dark"></div>
+        <div className="container mx-auto">
+          <div className="pt-48 pb-20">
+            <div className="grid lg:grid-cols-2 gap-2 py-10">
+              <div className="px-6">
+                <div className="flex flex-wrap items-center">
+                  <div className="w-1/2">
+                    <div className="flex flex-col">
+                      <div className="w-full mt-4 pt-2">
+                        <div className="shadow-img bg-white rounded shadow-xl overflow-hidden mr-5">
+                          <img src={Abt1} className="w-full" alt="Loading" />
+                          <div className="bg-dark"></div>
+                        </div>
                       </div>
-                    </div>
-                    <div class="w-full mt-4 pt-2 text-right">
-                      <a
-                        href="#/"
-                        class="btn btn-info bg-[#033540] text-white mr-5 px-4 py-2 rounded-lg"
-                      >
-                        Read More <i class="mdi mdi-chevron-right"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="w-1/2">
-                  <div class="flex flex-col">
-                    <div class="w-full">
-                      <div class="shadow-img bg-white rounded shadow-xl overflow-hidden">
-                        <img src={Abt2} class="w-full" alt="Loading" />
-                        <div class="bg-dark"></div>
-                      </div>
-                    </div>
-                    <div class="w-full mt-4 pt-2">
-                      <div class="shadow-img bg-white rounded shadow-xl overflow-hidden">
-                        <img src={Abt3} class="w-full" alt="Loading" />
-                        {/* <div class="bg-dark"></div> */}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="px-4 pt-10">
-              <div class="lg:ml-5">
-                <h1 class="text-3xl font-bold mb-3">About Us</h1>
-                <h4 class="mb-4 font-medium">
-                  Our mission is to  make your life easier.
-                </h4>
-                <p class="text-gray-600 mb-0">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit quod
-                  debitis praesentium pariatur temporibus ipsa, cum quidem
-                  obcaecati sunt?
-                </p>
-                <div class="flex flex-wrap">
-                  {AboutCard.map((link,index) => (
-                    <div class="shadow-img w-full lg:w-2/5 mt-4 pt-2 lg:mr-5">
-                    <div class="flex items-center rounded shadow-lg p-3 bg-white">
-                      <div className="h-4 mb-2 text-2xl text-color-sherpa-blue">
-                        {/* <FaPlay /> */}
-                        {link.icon}
-                      </div>
-                      <h6 class="ml-3 mb-0">
-                        <a href="#/" class="text-dark">
-                          {/* Responsive */}
-                          {link.title}
+                      <div className="w-full mt-4 pt-2 text-right">
+                        <a
+                          href="#/"
+                          className="btn btn-info background-color-sherpa-blue text-white mr-5 px-4 py-2 rounded-lg"
+                        >
+                          Read More <i className="mdi mdi-chevron-right"></i>
                         </a>
-                      </h6>
+                      </div>
                     </div>
                   </div>
-                  ))}
+
+                  <div className="w-1/2">
+                    <div className="flex flex-col">
+                      <div className="w-full">
+                        <div className="shadow-img bg-white rounded shadow-xl overflow-hidden">
+                          <img src={Abt2} className="w-full" alt="Loading" />
+                          <div className="bg-dark"></div>
+                        </div>
+                      </div>
+                      <div className="w-full mt-4 pt-2">
+                        <div className="shadow-img bg-white rounded shadow-xl overflow-hidden">
+                          <img src={Abt3} className="w-full" alt="Loading" />
+                          {/* <div className="bg-dark"></div> */}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="px-4 pt-10">
+                <div className="lg:ml-5">
+                  <h1 className="text-3xl font-bold mb-3">About Us</h1>
+                  <h4 className="mb-4 font-medium">
+                    Our mission is to make your life easier.
+                  </h4>
+                  <p className="text-gray-600 mb-0">
+                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati, beatae excepturi? Nobis veritatis nihil officia assumenda soluta eligendi. Odit et architecto placeat nisi, perspiciatis iste quo, molestias a tenetur neque nemo, excepturi dolores magnam porro doloribus id officia veniam? Deserunt animi eos cupiditate exercitationem eligendi totam modi ipsa nihil dolorum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati, beatae excepturi? Nobis veritatis nihil officia assumenda soluta eligendi. Odit et architecto placeat nisi, perspiciatis iste quo, molestias a tenetur neque nemo, excepturi dolores magnam porro doloribus id officia veniam? Deserunt animi eos cupiditate exercitationem eligendi totam modi ipsa nihil dolorum.doloribus id officia veniam? Deserunt animi eos cupiditate exercitationem eligendi totam modi ipsa nihil dolorum.
+                  </p>
+                  <div className="flex flex-wrap">
+                    {AboutCard.map((link, index) => (
+                      <div className="shadow-img w-full lg:w-2/5 mt-4 pt-2 lg:mr-5">
+                        <div className="flex items-center rounded shadow-lg p-3 bg-white">
+                          <div className="h-4 mb-2 text-2xl text-color-sherpa-blue">
+                            {/* <FaPlay /> */}
+                            {link.icon}
+                          </div>
+                          <h6 className="ml-3 mb-0">
+                            <a href="#/" className="text-dark">
+                              {/* Responsive */}
+                              {link.title}
+                            </a>
+                          </h6>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
+
+            {/* cards */}
+            {/* <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-8 px-6">
+              <div class="shadow-img rounded p-5 flex flex-col items-center">
+                <FaUsersCog className="text-5xl font-bold" />
+                <div
+                  class="font-bold my-3 text-3xl counter"
+                  data-target="12000"
+                ></div>
+                <span className="text-xl">Experts</span>
+              </div>
+              <div class="shadow-img rounded p-5 flex flex-col items-center">
+                <FaThumbsUp className="text-5xl font-bold" />
+                <div
+                  class="font-bold my-3 text-3xl counter"
+                  data-target="5000"
+                ></div>
+                <span className="text-xl">Happy Clients</span>
+              </div>
+              <div class="shadow-img rounded p-5 flex flex-col items-center">
+                <FaUsers className="text-5xl font-bold" />
+                <div
+                  class="font-bold my-3 text-3xl counter"
+                  data-target="7500"
+                ></div>
+                <span className="text-xl">Partners</span>
+              </div>
+              <div class="shadow-img rounded p-5 flex flex-col items-center">
+                <FaTrophy className="text-5xl font-bold" />
+                <div
+                  class="font-bold my-3 text-3xl counter"
+                  data-target="5000"
+                ></div>
+                <span className="text-xl">IT consultant</span>
+              </div>
+            </div> */}
           </div>
-          
-               {/* cards */}
-             <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 py-12 gap-8 mx-10">
-            <div class="shadow-img rounded p-5 flex flex-col items-center">
-              <FaUsersCog className="text-5xl font-bold" />
-              <div
-                class="font-bold my-3 text-3xl counter"
-                data-target="12000"
-              ></div>
-              <span className="text-xl">Experts</span>
-            </div>
-            <div class="shadow-img rounded p-5 flex flex-col items-center">
-              <FaThumbsUp className="text-5xl font-bold" />
-              <div
-                class="font-bold my-3 text-3xl counter"
-                data-target="5000"
-              ></div>
-              <span className="text-xl">Happy Clients</span>
-            </div>
-            <div class="shadow-img rounded p-5 flex flex-col items-center">
-              <FaUsers className="text-5xl font-bold" />
-              <div
-                class="font-bold my-3 text-3xl counter"
-                data-target="7500"
-              ></div>
-              <span className="text-xl">Partners</span>
-            </div>
-            <div class="shadow-img rounded p-5 flex flex-col items-center">
-              <FaTrophy className="text-5xl font-bold" />
-              <div
-                class="font-bold my-3 text-3xl counter"
-                data-target="5000"
-              ></div>
-              <span className="text-xl">IT consultant</span>
-            </div>
-          </div>
-      
         </div>
       </div>
     </>
